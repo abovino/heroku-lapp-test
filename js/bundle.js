eca7f7e80572ee66a71dcb7ee2eef92023f81f47
Angelo Bovino <angelo.bovino@outlook.com>
2017-10-24 00:19:41 -0400

    Latest build with live http requests to api

--- js/bundle.js
+++ js/bundle.js
@@ -9368,8 +9368,8 @@ var Unitronic = function (_React$Component) {
 								contentType: "application/json; charset=utf-8",
 								dataType: "JSON",
 								type: "GET",
-								// url: "https://selectortoolapi.com/bus/data"
-								url: "http://localhost:3000/bus/data"
+								url: "https://selectortoolapi.com/bus/data"
+								// url: "http://localhost:3000/bus/data"
 						}).done(function (data) {
 								console.log(data);
 						}).then(function (data) {
@@ -11344,8 +11344,8 @@ var Etherline = function (_React$Component) {
 				contentType: "application/json; charset=utf-8",
 				dataType: "JSON",
 				type: "GET",
-				// url: "https://selectortoolapi.com/etherline/data"
-				url: "http://localhost:3000/etherline/data"
+				url: "https://selectortoolapi.com/etherline/data"
+				// url: "http://localhost:3000/etherline/data"
 			}).done(function (data) {
 				console.log(data);
 			}).then(function (data) {
